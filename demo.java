9fe4eecbb6ebca4fb02ecbcb4ebd3ccfe96637de
anupam888 <61756205+anupam888@users.noreply.github.com>
2020-04-28 15:16:53 +0530

    code is commited

--- demo.java
+++ demo.java
@@ -3,6 +3,9 @@ class demo  {
         public static void main ()
         {
                 s.o.print("hello world");
+            s.o.print("hello world");
+            s.o.print("hello world");
+            s.o.print("hello world");
         }
 
 }
